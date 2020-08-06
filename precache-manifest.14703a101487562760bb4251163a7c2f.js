fb5f7b0a20b24395f7d7b8517908557c4d75ddd2
svvimming <benmunozj@gmail.com>
2020-08-06 09:39:50 -0400

    Updates

---
+++ precache-manifest.14703a101487562760bb4251163a7c2f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715efc8dddb7adb5fc4d070af6178628",
+    "url": "/blob-ecology/index.html"
+  },
+  {
+    "revision": "6f4168aa953637f8ec4f",
+    "url": "/blob-ecology/static/css/main.62469909.chunk.css"
+  },
+  {
+    "revision": "26c8003bf7e95d2c9e82",
+    "url": "/blob-ecology/static/js/2.d7c9a1c5.chunk.js"
+  },
+  {
+    "revision": "e9c6377cd36ae7aafd3f34018dc4ae75",
+    "url": "/blob-ecology/static/js/2.d7c9a1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4168aa953637f8ec4f",
+    "url": "/blob-ecology/static/js/main.e4d67f3f.chunk.js"
+  },
+  {
+    "revision": "10e01077471cb1fef872",
+    "url": "/blob-ecology/static/js/runtime-main.ee5d7646.js"
+  },
+  {
+    "revision": "c3842ed2ee36521d9006a2360f2b038b",
+    "url": "/blob-ecology/static/media/bubble.c3842ed2.png"
+  },
+  {
+    "revision": "8db3d1de239239c7fd3ed70dc7302464",
+    "url": "/blob-ecology/static/media/chandelier.8db3d1de.mp3"
+  },
+  {
+    "revision": "60496b5a3d48d63c62ad6cf9a4a630b2",
+    "url": "/blob-ecology/static/media/dots3.60496b5a.png"
+  },
+  {
+    "revision": "6be5dfa6924bd2fea011639114bac8f6",
+    "url": "/blob-ecology/static/media/fung.6be5dfa6.png"
+  },
+  {
+    "revision": "7a6f84e033b240744a05d8309a90477d",
+    "url": "/blob-ecology/static/media/glassy.7a6f84e0.mp3"
+  },
+  {
+    "revision": "19f08092e0c680aa8b159d8968020238",
+    "url": "/blob-ecology/static/media/irridescent.19f08092.png"
+  },
+  {
+    "revision": "9c232a7f25b01307f7aa84ad95453f73",
+    "url": "/blob-ecology/static/media/mew-sat-r.9c232a7f.png"
+  },
+  {
+    "revision": "404bb102b45703fc2b4819ad4c28844b",
+    "url": "/blob-ecology/static/media/nban-squidge.404bb102.png"
+  },
+  {
+    "revision": "f9c82a71fe0acc44babc1447762e19e7",
+    "url": "/blob-ecology/static/media/octo.f9c82a71.png"
+  },
+  {
+    "revision": "74e3802d70677fbd42ebf85a415ae34b",
+    "url": "/blob-ecology/static/media/orbitals.74e3802d.gif"
+  },
+  {
+    "revision": "f12d14b51c2923d706f35957c8920dad",
+    "url": "/blob-ecology/static/media/ovaling-short.f12d14b5.mp3"
+  },
+  {
+    "revision": "30a110519c5cd15dcdef3a8c4e00050c",
+    "url": "/blob-ecology/static/media/psychic-life.30a11051.mp3"
+  },
+  {
+    "revision": "5a5e59650a122f0135447b1646a95a2e",
+    "url": "/blob-ecology/static/media/sinkcin.5a5e5965.png"
+  },
+  {
+    "revision": "3b11db114bce0b4af1a26b18061c713c",
+    "url": "/blob-ecology/static/media/slant.3b11db11.jpg"
+  },
+  {
+    "revision": "d11d87c4b5a9f47bfed33dca98f3753d",
+    "url": "/blob-ecology/static/media/slopoke.d11d87c4.mp3"
+  },
+  {
+    "revision": "3e19d6f7ed78bb1c2bab3da86c331216",
+    "url": "/blob-ecology/static/media/svvities.3e19d6f7.mp3"
+  },
+  {
+    "revision": "3c8d1bd2562b5b3e35224b6e9b7b2722",
+    "url": "/blob-ecology/static/media/yell.3c8d1bd2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blob-ecology/precache-manifest.1de6e05add481c5fddcefc81e36ee81b.js"
+  "/blob-ecology/precache-manifest.14703a101487562760bb4251163a7c2f.js"
 );
 
 self.addEventListener('message', (event) => {
